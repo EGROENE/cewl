61cdabf1adbbb58bdf3f723b5fd7b32e4e472b68
Ethan Groene <ethangroene@gmail.com>
2022-08-03 14:40:16 -0500

    Add correct JS w/ some styling

--- main.js
+++ main.js
@@ -13,15 +13,28 @@ const styleSelectedPageNavLink = () => {
 // If not possible to populate html using js, make a separate function for each upcoming event & run upon pageload
 // Array of objects containing info for each event:
 const events = [
-    { eventHeader: "4th-Of-July Cookout", eventTime: "4:00pm", eventPlace: "Liberty Park" },
-    { eventHeader: "3-on-3 Basketball Tournament", eventTime: "10:00am", eventPlace: "Umlauf Middle School" }
+    { eventHeader: "4th-Of-July Cookout", eventDate: "July 4", eventTime: "4:00pm", eventPlace: "Liberty Park" },
+    { eventHeader: "3-on-3 Basketball Tournament", eventDate: "July 23", eventTime: "10:00am", eventPlace: "Umlauf Middle School" }
 ]
-
 // Function to populate events.html (run upon loading of page):
 const populateEvents = () => {
     for (const event of events) {
-        document.getElementById('event-header').innerHTML = event.eventHeader;
-        document.getElementById('event-time').innerHTML = event.eventTime;
-        document.getElementById('event-place').innerHTML = event.eventPlace;
+        document.getElementById('events-two').innerHTML += '<a href="#" class="event">' 
+        + '<header>' 
+        + event.eventHeader 
+        + '</header>' 
+        + '<p>' 
+        + event.eventDate
+        + '</p>'
+        + '<p>' 
+        + event.eventTime
+        + '</p>'
+        + '<p>' 
+        + event.eventPlace
+        + '</p>'
+        + '</a>';
+        
+    } 
+        /* document.getElementById('event-time').innerHTML = event.eventTime;
+        document.getElementById('event-place').innerHTML = event.eventPlace; */
     }
-}
