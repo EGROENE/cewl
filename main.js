334c4ca876b695be73a68be23305034e59e7b903
Ethan Groene <ethangroene@gmail.com>
2022-08-03 00:15:12 -0500

    Reset main.js

--- main.js
+++ main.js
@@ -6,16 +6,3 @@ const styleSelectedPageNavLink = () => {
         element.style.borderBottom = 'var(--size-4) solid var(--secondary-color)';
     }
 }
-
-// EVENTS PAGE
-// Array of objects containing info for each event:
-const events = [
-    { eventHeader: "4th-Of-July Cookout", eventTime: "4:00pm", eventPlace: "Liberty Park" },
-    { eventHeader: "3-on-3 Basketball Tournament", eventTime: "10:00am", eventPlace: "Umlauf Middle School" }
-]
-
-// Function to populate events.html (run upon loading of page):
-let html = ""
-const populateEvents = () => {
-    
-}
